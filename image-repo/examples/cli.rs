86cf1cc97e0ee517ed84847c770722c7d7fc05e1
Mat Jones <mat@mjones.network>
2023-11-04 13:06:40 -0400

    update example

--- image-repo/examples/cli.rs
+++ image-repo/examples/cli.rs
@@ -3,9 +3,19 @@ use std::env;
 
 #[tokio::main]
 pub async fn main() {
-    for url in env::args().skip(1) {
+    let urls = env::args().skip(1);
+
+    if urls.len() == 0 {
+        eprintln!("Usage: cargo run -p image-repo --example cli -- [url]");
+    }
+
+    for url in urls {
         println!("Attempting to download repo from {url} ...");
         let result = download_repo_manifest(url).await;
-        println!("Result:\n\n{result:?\n\n}");
+        if let Ok(repo) = result {
+            println!("{}", serde_json::to_string_pretty(&repo).unwrap());
+        } else {
+            println!("{result:?}");
+        }
     }
 }
