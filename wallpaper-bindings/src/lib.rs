937808668f8587b91618c4962ecd7e4bb344a94d
Robert Jones <rjones38@ycp.edu>
2023-11-09 19:51:18 -0500

    Added library to toml file as well as an executable, moved OS logic over to lib.rs

--- wallpaper-bindings/src/lib.rs
+++ wallpaper-bindings/src/lib.rs
@@ -1,8 +1,13 @@
 use more_wallpapers::Mode;
 
-#[tauri::command]
 pub fn set_wallpaper_from_file(file_path: &str) {
+    set_wallpaper(file_path)
+}
+
+#[tauri::command]
+fn set_wallpaper(file_path: &str) {
     println!("Setting wallpaper from file: {}", file_path);
     let images = vec![file_path];
+
     let _ = more_wallpapers::set_wallpapers_from_vec(images, "", Mode::Crop);
 }
--- wallpaper-bindings/src/main.rs
+++ wallpaper-bindings/src/main.rs
@@ -1,11 +1,7 @@
-mod tauri_bridge;
 use std::env;
+use jdtwallpaper::set_wallpaper_from_file;
 
-pub fn set_wallpaper(filepath: &str){
-    tauri_bridge::set_wallpaper_from_file(filepath);
-}
-
-fn main() {
+pub fn main() {
     let file_path = env::args().skip(1).next().unwrap();
-    set_wallpaper(file_path.as_str());
+    set_wallpaper_from_file(file_path.as_str());
 }
