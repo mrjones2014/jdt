7cbadea5db6ca1954039422b032aea78ce426c87
Robert Jones <rjones38@ycp.edu>
2023-11-14 17:18:10 -0500

    Applying some feedback :)

--- wallpaper-bindings/src/lib.rs
+++ wallpaper-bindings/src/lib.rs
@@ -1,11 +1,11 @@
 use more_wallpapers::Mode;
+use std::path::PathBuf;
 
-pub fn set_wallpaper_from_file(file_path: &str) {
-    set_wallpaper(file_path)
+pub fn set_wallpaper_from_file(path: &PathBuf) {
+    set_wallpaper(path)
 }
 
-#[tauri::command]
-fn set_wallpaper(file_path: &str) {
+fn set_wallpaper(file_path: &PathBuf) {
     println!("Setting wallpaper from file: {}", file_path);
     let images = vec![file_path];
 
